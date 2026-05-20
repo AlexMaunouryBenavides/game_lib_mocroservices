7740f308fad903db60d02e222f752dc10656311a
maxime <maximelarquetoux@gmail.com>
2026-05-20 16:01:09 +0200

    feat: user controller created

--- gateway/src/user/user.controller.ts
+++ gateway/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject, OnModuleInit } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Inject, OnModuleInit, Patch, Post, Put } from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
 import { firstValueFrom } from "rxjs";
 
@@ -10,9 +10,28 @@ export class UsersController implements OnModuleInit {
         await this.client.connect();
     }
 
+    @Post()
+    async create(@Body() userData: any) {
+        return firstValueFrom(this.client.send({ cmd: 'createUser' }, userData));
+    }
+
     @Get()
     async findAll() {
         return firstValueFrom(this.client.send({ cmd: 'findAllUsers' }, {}));
     }
 
+    @Get(':id')
+    async findOne(id: number) {
+        return firstValueFrom(this.client.send({ cmd: 'findOneUser' }, id));
+    }  
+
+    @Patch(':id')
+    async update(id: number, @Body() userData: any) {
+        return firstValueFrom(this.client.send({ cmd: 'updateUser' }, { id, userData }));
+    }
+
+    @Delete(':id')
+    async remove(id: number) {
+        return firstValueFrom(this.client.send({ cmd: 'removeUser' }, id));
+    }   
 }
